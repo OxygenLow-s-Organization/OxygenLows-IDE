a4a0d400ba71f3b671d3fc037b97e324b3565a39
Oxygen-Low <95589118+Oxygen-Low@users.noreply.github.com>
2025-09-10 10:32:20 +0000

    Changed minimize button.

--- src/TitleBar.js
+++ src/TitleBar.js
@@ -6,7 +6,7 @@ export default function TitleBar() {
     <div className="title-bar" style={{ WebkitAppRegion: 'drag' }}>
       <span className="title">OxygenLows IDE</span>
       <div className="window-controls" style={{ WebkitAppRegion: 'no-drag' }}>
-        <button onClick={() => ipcRenderer.send('window-minimize')}>_</button>
+        <button onClick={() => ipcRenderer.send('window-minimize')}>-</button>
         <button onClick={() => ipcRenderer.send('window-maximize')}>▢</button>
         <button onClick={() => ipcRenderer.send('window-close')}>×</button>
       </div>
